84e17f660e9a756ec565a7cd17e26be6823226b6
Yuval Simon <63560633+Reven8e@users.noreply.github.com>
2020-10-09 14:03:22 +0300

    Update app.py
    Fixed captcha checker.
    Removed login id.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, session, render_template, flash, send_file
+from flask import Flask, request, session, render_template, send_file, flash, redirect, url_for, json, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from io import BytesIO
-
+import requests
 
 # configs
 app = Flask(__name__)
@@ -30,6 +30,15 @@ class storage(db.Model):
     def __repr__(self):
         return f'{self.name} id= ' + str(self.Sid)
 
+
+class recaptcha(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    session_token = db.Column(db.String(12), unique=True)
+    session_token_status = db.Column(db.String)
+
+    def __repr__(self):
+        return f'{str(self.session_token)}'
+        
 
 # routes
 
@@ -85,40 +94,54 @@ def admin_register(ad_uname, ad_password, ad_token):
         return 'You already logged in'
 
 
-@app.route('/login/<string:l_uname>/<string:l_password>/<int:l_id>', methods=['POST', 'GET'])
-def login(l_uname, l_password, l_id):
+@app.route('/login/<string:l_uname>/<string:l_password>', methods=['POST', 'GET'])
+def login(l_uname, l_password):
 
     if "s_uname" not in session and "s_password" not in session:
 
-        try:
+        check_uname = auth.query.filter_by(uname=l_uname).first()
+        check_password = auth.query.filter_by(password=l_password).first()
 
-            check_id = auth.query.get_or_404(l_id)
-            check_uname = auth.query.get(l_id).uname
-            check_password = auth.query.get(l_id).password
+        if check_uname and check_password:
 
-            if auth.query.get(l_id).uname == l_uname and auth.query.get(l_id).password == l_password:
+            session['s_uname'] = l_uname
+            session['s_password'] = l_password
+            status = 'logged in as ' + l_uname
+            
+        else:
 
-                check_admin = auth.query.get(l_id).admin
-
-                if check_admin == 'True':
-
-                    session['admin_session_uname'] = check_uname
-                    session['admin_session_password'] = check_password
-                    return 'Hello admin, ' + check_uname
-
-                elif check_admin == 'False':
-
-                    session['s_uname'] = check_uname
-                    session['s_password'] = check_password
-                    return 'logged in as ' + check_uname
-
-        except:
-
-            return 'username or password or id is not currect'
+            status = 'Username or password not found in our database'
 
     elif "s_uname" in session and "s_password" in session:
 
-        return "Your already logged in."
+        status =  "Your already logged in."
+    
+    return (status)
+
+
+@app.route('/login/admin/<string:ad_l_uname>/<string:ad_l_password>/<ad_token>', methods=['POST', 'GET'])
+def admin_login(ad_l_uname, ad_l_password, ad_token):
+
+    if "admin_session_uname" not in session and "admin_session_password" not in session:
+
+        check_uname = auth.query.filter_by(uname=ad_l_uname).first()
+        check_password = auth.query.filter_by(password=ad_l_password).first()
+
+        if check_uname and check_password:
+
+            session["admin_session_uname"] = ad_l_uname
+            session["admin_session_password"] = ad_l_password
+            status = 'logged in as ' + ad_l_uname
+            
+        else:
+
+            status = 'Username or password not found in our database'
+
+    elif "admin_session_uname" in session and "admin_session_password" in session:
+
+        status =  "Your already logged in."
+    
+    return (status)
 
 
 @app.route('/logout', methods=['POST', 'GET'])
@@ -272,5 +295,63 @@ def all_uploads():
 
 
 
+# Reaptcha
+
+
+def is_human(captcha_response):
+    """ Validating recaptcha response from google server
+        Returns True captcha test passed for submitted form else returns False.
+    """
+    secret = "6LejatIZAAAAAOLtcBHyIAcDeE36fnQZmmo_bV7B"
+    payload = {'response':captcha_response, 'secret':secret}
+    response = requests.post("https://www.google.com/recaptcha/api/siteverify", payload)
+    response_text = json.loads(response.text)
+    return response_text['success']
+
+
+@app.route('/recaptcha/verify/<string:token>', methods=['GET', 'POST'])
+def captcha(token):
+
+    sitekey = "6LejatIZAAAAAJ9TCQrLDS_flYHEknmnNFHqfcpM"
+    token_checker = recaptcha.query.all()
+
+    try:
+
+        if request.method == "POST":
+
+            captcha_response = request.form['g-recaptcha-response']
+
+            if is_human(captcha_response):
+                
+                status = "Verification successful. You may now close this window."
+                new_session_token = recaptcha(session_token=token, session_token_status = 'Successed')
+                db.session.add(new_session_token)
+                db.session.commit()
+                
+            else:
+
+                status = "Sorry! Please Check Im not a robot again."
+
+            flash(status)
+
+    except:
+        flash ('This token in not valid or has already been used, please request new token.')
+
+    return render_template("captcha.html", sitekey=sitekey)
+
+
+@app.route('/api/recaptcha/check/<string:token>', methods=['GET', 'POST'])
+def protected(token):
+
+    find_token = recaptcha.query.filter_by(session_token=token).first()
+    if find_token:
+        status = 'Verfication successed'
+    else:
+        status = 'Verfication failed'
+    
+    return (status)
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='localhost')
+    app.run(debug=True)
